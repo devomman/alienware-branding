d5c4f983e312357cb7a0a73ec55cfc92a760cb12
Muhammad Omman <devomman@gmail.com>
2021-10-30 15:30:35 +0300

    Update Clear.bat

--- Clear.bat
+++ Clear.bat
@@ -2,7 +2,7 @@
 
 :: (Run as Admin code starts)
 REM  Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+>nul 2>&1 "%SYSTEMROOT%\system32\icacls.exe" "%SYSTEMROOT%\system32\config\system"
 
 REM If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
